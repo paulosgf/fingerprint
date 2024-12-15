970b5828962ec2de561b6f088f5e4d4e8d0af539
Paulo Ferraz <paulosgf.pf@gmail.com>
2024-12-15 00:35:44 -0300

    Add subprojects support

---
+++ src/auth/auth.pro
@@ -0,0 +1,24 @@
+QT += core gui widgets
+TARGET = auth
+
+INCLUDEPATH += ../
+DEPENDPATH += ../
+VPATH += ../../bin ../../lib
+QMAKE_CXXFLAGS += -Wall -O2
+QMAKE_LFLAGS += -Wl,-rpath,/usr/local/lib
+
+SOURCES += main.cpp mainwindow.cpp
+
+HEADERS += ../mainwindow.h
+
+FORMS += ../mainwindow.ui
+
+LIB_NAME = libfpengine.so
+DESTDIR = ../../bin/
+BIN_DIR = /usr/local/bin/
+LIBS += -L../../lib -lfpengine -lrt -ldl
+
+target.files = $$TARGET
+target.path = $$BIN_DIR
+library.files = $$LIB_NAME
+INSTALLS = target
---
+++ src/fingerprint/main.cpp
@@ -0,0 +1,38 @@
+#include <QApplication>
+#include "../mainwindow.h"
+#include <iostream>
+#include <cstdlib>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <string.h>
+
+
+int createEnv()
+{
+    DIR *dirptr;
+    char *home = getenv("HOME");
+
+    dirptr = opendir(home);
+    if (dirptr == NULL) {
+        puts("User directory not found! Creating this...\n");
+        printf("%s\n", home);
+        if (mkdir(home, 0777) != 0)    {
+            printf("Cannot create directory. Error: %s\n", strerror(errno));
+            return 1;
+        }
+    } else {
+        closedir(dirptr);
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    createEnv();
+    w.show();
+    
+    return a.exec();
+}
