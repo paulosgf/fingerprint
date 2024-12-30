f1b20fc84183adbbb1ca35289b0624e96a3633f3
Paulo Ferraz <paulosgf.pf@gmail.com>
2024-12-29 21:18:43 -0300

    Initial coding of auth application

--- src/fingerprint/main.cpp
+++ src/fingerprint/main.cpp
@@ -5,6 +5,31 @@
 #include <QApplication>
 #include <QLocale>
 #include <QTranslator>
+#include <iostream>
+#include <cstdlib>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <string.h>
+
+int createEnv()
+{
+    DIR *dirptr;
+    char *home = getenv("HOME");
+
+    dirptr = opendir(home);
+    if (dirptr == NULL) {
+        puts("User directory not found! Creating this...\n");
+        printf("%s\n", home);
+        if (mkdir(home, 0777) != 0)    {
+            printf("Cannot create directory. Error: %s\n", strerror(errno));
+            return 1;
+        }
+    } else {
+        closedir(dirptr);
+    }
+    return 0;
+}
 
 int main(int argc, char *argv[])
 {
@@ -49,7 +74,9 @@ int main(int argc, char *argv[])
             break;
         }
     }
+
     MainWindow wdw;
+    createEnv();
     wdw.show();
     return app.exec();
 }
